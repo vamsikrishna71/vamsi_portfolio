8cc5016a1d46b3c5ed78538ccf5bfb88f335ef08
vamsikrishna71 <vamsicse0@gmail.com>
2023-06-02 09:30:21 +0530

    feat:works tilt

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Tilt from "react-tilt";
-import { motion } from "framer-motion";
+import React from "react"
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
 
-import { styles } from "../styles";
-import { github } from "../assets";
-import { SectionWrapper } from "../hoc";
-import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
+import { styles } from "../styles"
+import { github } from "../assets"
+import { SectionWrapper } from "../hoc"
+import { projects } from "../constants"
+import { fadeIn, textVariant } from "../utils/motion"
 
 const ProjectCard = ({
   index,
@@ -64,8 +64,8 @@ const ProjectCard = ({
         </div>
       </Tilt>
     </motion.div>
-  );
-};
+  )
+}
 
 const Works = () => {
   return (
@@ -94,7 +94,7 @@ const Works = () => {
         ))}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default SectionWrapper(Works, "");
+export default SectionWrapper(Works, "")
